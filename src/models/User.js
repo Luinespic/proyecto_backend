df54dda9aa1ef3c65ce6db02081062f2ea390fe9
Luinespic <lucia_49_92@hotmail.com>
2026-06-29 19:02:44 +0200

    feat: user model

---
+++ src/models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "El nombre de usuario es obligatorio"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "El correo electrónico es obligatorio"],
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, "La contraseña es obligatoria"],
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    image: {
+      type: String,
+      required: [true, "La imagen de perfil es obligatoria"],
+    },
+    coursesEnrolled: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  },
+);
